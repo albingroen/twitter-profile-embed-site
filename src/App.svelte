895321893e139e170f557bfadd0fe6945a63ceae
Albin Groen <albin.groen@gmail.com>
2021-12-27 20:52:36 +0100

    Set up and run formatter

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,10 @@
 <script>
-	import Seo from "./Seo.svelte";
+  import Seo from "./Seo.svelte";
 
-	const API_URL = "https://twitter-profile-embed.vercel.app";
+  const API_URL = "https://twitter-profile-embed.vercel.app";
 
-	let inputValue = "";
-	let username = "";
+  let inputValue = "";
+  let username = "";
 </script>
 
 <Seo />
@@ -13,67 +13,67 @@
 <p id="description">Enter your username to preview your Twitter profile.</p>
 
 <form
-	on:submit={(e) => {
-		e.preventDefault();
-		username = inputValue;
-	}}
+  on:submit={(e) => {
+    e.preventDefault();
+    username = inputValue;
+  }}
 >
-	<input
-		bind:value={inputValue}
-		placeholder="tim_cook"
-		id="search-input"
-		type="text"
-	/>
-	<button type="submit">Preview profile</button>
+  <input
+    bind:value={inputValue}
+    placeholder="tim_cook"
+    id="search-input"
+    type="text"
+  />
+  <button type="submit">Preview profile</button>
 </form>
 
 {#if username}
-	<iframe
-		title={`${username} Twitter profile`}
-		src={`${API_URL}?username=${username}`}
-		frameBorder="0"
-		height="420px"
-		width="480px"
-	/>
+  <iframe
+    title={`${username} Twitter profile`}
+    src={`${API_URL}?username=${username}`}
+    frameBorder="0"
+    height="420px"
+    width="480px"
+  />
 
-	<code>
-		{`<iframe width="480px" height="420px" frameBorder="0" src="${API_URL}?username=${username}" />`}
-	</code>
-	<p id="code-hint">PS. You might have to adjust the dimensions a bit.</p>
+  <code>
+    {`<iframe width="480px" height="420px" frameBorder="0" src="${API_URL}?username=${username}" />`}
+  </code>
+  <p id="code-hint">PS. You might have to adjust the dimensions a bit.</p>
 {/if}
 
 <style>
-	code {
-		line-height: 1.5;
-		border-radius: 5px;
-		background: #eee;
-		max-width: 480px;
-		padding: 0.5rem;
-		display: block;
-	}
+  code {
+    line-height: 1.5;
+    border-radius: 5px;
+    background: #eee;
+    max-width: 480px;
+    padding: 0.5rem;
+    display: block;
+  }
 
-	#description {
-		margin-top: 1rem;
-	}
+  #description {
+    margin-top: 1rem;
+  }
 
-	#search-input,
-	#code-hint,
-	iframe,
-	code {
-		margin-top: 1.5rem;
-	}
+  #search-input,
+  #code-hint,
+  iframe,
+  code {
+    margin-top: 1.5rem;
+  }
 
-	iframe {
-		border: 1px solid #eee;
-	}
+  iframe {
+    border: 1px solid #eee;
+  }
 
-	@media (prefers-color-scheme: dark) {
-		code {
-			background: #1f2937;
-		}
+  @media (prefers-color-scheme: dark) {
+    code {
+      background: #1f2937;
+    }
 
-		iframe {
-			border: 1px solid #374151;
-		}
-	}
+    iframe {
+      border: 1px solid #374151;
+    }
+  }
 </style>
